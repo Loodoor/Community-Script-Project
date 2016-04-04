0a4ee36e5b8619855c0964851e17b48934c6ea07
Loodoor <folaefolc@outlook.fr>
2016-04-04 14:24:23 +0200

    Created template PHP file (index)

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>CommunityScriptProject</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Language" content="fr-FR" />
+        <meta name="robots" content="all" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="description" content="" />
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- Website Style -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!-- Petit template pour le "head" -->
+        <div class="jumbotron">
+            <div class="login-btn col-md-12">
+                <a class="btn btn-primary btn-xs" href="index.php?action=ucp">Pseudo</a>
+            </div>
+            <h1 onclick="window.location = 'index.php?action=forum';">CommunityScriptProject</h1>
+            <h3>Slogan du site</h3>
+        </div>
+        
+        <!-- Le content qui change serait situé dans cette partie -->
+        <div class="container">
+            Le reste du site
+        </div>
+    </body>
+</html>
